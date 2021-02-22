f3873257a174789cc14d0bebbc4edeee189cc54e
Jayden Shaw <jayden.shaw@gmail.com>
2021-02-22 10:16:41 -0700

    Updated README document.

--- src/pages/Contact/Contact.page.js
+++ src/pages/Contact/Contact.page.js
@@ -8,7 +8,7 @@ function Contact() {
             <div className='contact'>
                 OneMust.Live is currently under construction.<br />
                 For more information contact us <a href='mailto:jayden.shaw@gmail.com'>via email</a><br />
-                Or look for updates at our <a href='#'>GitHub Repository</a>
+                Or look for updates at our <a href='https://github.com/jshaw990/onemust.live'>GitHub Repository</a>
             </div>
         </div>
     )
